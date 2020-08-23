b95bdca78299b83961ce2fdf943d5f398b706be9
resueman14 <nowruz.k@yandex.ru>
2020-08-23 22:07:40 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace win_shutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_abort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timePicker
+            // 
+            this.timePicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timePicker.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.timePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.Location = new System.Drawing.Point(0, 0);
+            this.timePicker.MinDate = new System.DateTime(1995, 8, 23, 0, 0, 0, 0);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.Size = new System.Drawing.Size(310, 83);
+            this.timePicker.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SHUTDOWN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_abort
+            // 
+            this.button_abort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_abort.Location = new System.Drawing.Point(0, 178);
+            this.button_abort.Name = "button_abort";
+            this.button_abort.Size = new System.Drawing.Size(310, 51);
+            this.button_abort.TabIndex = 4;
+            this.button_abort.Text = "ABORT";
+            this.button_abort.UseVisualStyleBackColor = true;
+            this.button_abort.Click += new System.EventHandler(this.button_abort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 231);
+            this.Controls.Add(this.button_abort);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.timePicker);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Windows shutdown gui app";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_abort;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace win_shutdown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timePicker.Format = DateTimePickerFormat.Time;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var psi = new ProcessStartInfo("shutdown", "/s /t " +
+                Convert.ToInt32(timePicker.Value.Subtract(DateTime.Now).TotalSeconds).ToString() );
+            psi.CreateNoWindow = false;
+            psi.UseShellExecute = true;
+            Process.Start(psi);
+        }
+
+        private void button_abort_Click(object sender, EventArgs e)
+        {
+            var psi = new ProcessStartInfo("shutdown", "/a");
+            psi.CreateNoWindow = false;
+            psi.UseShellExecute = true;
+            Process.Start(psi);
+        }
+    }
+}
